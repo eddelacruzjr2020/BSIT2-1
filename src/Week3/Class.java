66c9257490bc831a5799bd715765350e6d5e528b
Ed Dela Cruz <edmundo.delacruz@cartrack.com>
2024-03-23 10:59:57 +0800

    Week 3 additional code

---
+++ src/Week3/Class.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week3;
+
+/**
+ *
+ * @author Edmundo Dela Cruz
+ */
+public class Class {
+    
+    public void walk(){
+        System.out.println("Walk");
+    }
+}
