218506aa122d6be9fd5ad6ddeda7a197e1bfc086
Ed Dela Cruz <edmundo.delacruz@cartrack.com>
2024-04-06 09:29:04 +0800

    Week 4

---
+++ src/Week4/Car3Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week4;
+
+/**
+ *
+ * @author Edmundo Dela Cruz
+ */
+public class Car3Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Car3 car3 = new Car3("Honda", "Civic", 2024);
+        car3.vehicleDetails();
+        
+    }
+    
+}
