f1e5753aa78062d128bb4654d97c5548ddfdcc29
Ed Dela Cruz <edmundo.delacruz@cartrack.com>
2024-04-27 12:32:28 +0800

    Merge origin/Week6 into Week6
    # Conflicts:
    #	build.xml
    #	nbproject/build-impl.xml
    #	nbproject/genfiles.properties
    #	nbproject/project.properties
    #	nbproject/project.xml
    #	src/Week4/Canteen.java
    #	src/Week4/CanteenMain.java
    #	src/Week4/Car1.java
    #	src/Week4/Car1Main.java
    #	src/Week4/Car2.java
    #	src/Week4/Car2Main.java
    #	src/Week4/Car3.java
    #	src/Week4/Car3Main.java
    #	src/Week4/Car4Main.java
    #	src/Week5/A.java
    #	src/Week5/B.java
    #	src/Week5/C.java
    #	src/Week5/Car.java
    #	src/Week5/CarMain.java
    #	src/Week5/Hierarchical/A.java
    #	src/Week5/Hierarchical/B.java
    #	src/Week5/Hierarchical/C.java
    #	src/Week5/Hierarchical/D.java
    #	src/Week5/Hierarchical/Main.java
    #	src/Week5/Hybrid/A.java
    #	src/Week5/Hybrid/B.java
    #	src/Week5/Hybrid/C.java
    #	src/Week5/Hybrid/D.java
    #	src/Week5/Hybrid/Main.java
    #	src/Week5/Main.java
    #	src/Week5/Multiple/A.java
    #	src/Week5/Multiple/B.java
    #	src/Week5/Multiple/C.java
    #	src/Week5/SolarMain.java
    #	src/Week5/Vehicle.java
    #	src/Week6/Animal.java
    #	src/Week6/AnimalMain.java
    #	src/Week6/Calculator.java
    #	src/Week6/Cat.java
    #	src/Week6/Polygon.java
    #	src/Week6/Rectangle.java
    #	src/Week6/Triangle.java

---
+++ src/Week4/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week4;
+
+import Week5.Vehicle;
+
+/**
+ *
+ * @author Edmundo Dela Cruz
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("Ferrari", "Red", 2024);
+        vehicle.viewDetails();
+    }
+    
+}
